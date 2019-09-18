f1e8e9dd3fddd045e9af36059ac315e985191668
朱迪炜 <zhudiwei_13zhfx@126.com>
2019-09-18 18:06:15 +0800

    添加LengthOfLongestSubstring

--- src/main/java/LengthOfLongestSubstring.java
+++ src/main/java/LengthOfLongestSubstring.java
@@ -8,21 +8,21 @@ import java.util.Map;
  * @description
  * @date 2019/9/18 10:01
  * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * "pwwkew"   3
+ * left = Math.max(left,map.get(s.charAt(i)) + 1);
  */
 public class LengthOfLongestSubstring {
     public static int lengthOfLongestSubstring(String s) {
 
-        if (s==null || s.length()==0) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
 
         int j = 0;
-        char[] chars = s.toCharArray();
-        Map<Integer,Character> map =  new HashMap<>(8);
-        for (int i = 0; i < chars.length; i++) {
-            if (!map.containsValue(chars[i])) {
-                map.put(i,chars[i]);
-                j++;
+        Map<Character, Integer> map = new HashMap<>(8);
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j=Math.max();
             }
         }
 
@@ -30,7 +30,7 @@ public class LengthOfLongestSubstring {
     }
 
     public static void main(String[] args) {
-        String s= "pwwkew";
+        String s = "pwwkew";
         int i = lengthOfLongestSubstring(s);
         System.out.println("i = " + i);
     }
